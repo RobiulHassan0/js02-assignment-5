cfdeae5223c2a4f1709948b4278c6b35c4013534
RobiulHassan0 <robiulhassan270@gmail.com>
2025-04-29 02:56:46 +0600

    modal added

--- js/index.js
+++ js/index.js
@@ -1,15 +1,19 @@
 // Card 01
 document.getElementById('btn-donate-noakhali').addEventListener('click', function(){
     const donateForNoakhali = getInputValue('input-noakhali');
-    const donetedNoakhali = getTextValue('doneted-noakhali')
-    const bankMoney = getTextValue('bank-money')
+    const donetedNoakhali = getTextValue('doneted-noakhali');
+    const bankMoney = getTextValue('bank-money');
+
+
 
     if(!isNaN(donateForNoakhali) && donateForNoakhali > 0 && donateForNoakhali < bankMoney){
         const totalDonation = donetedNoakhali + donateForNoakhali;
         const remainingMoney = bankMoney - donateForNoakhali;
+
         document.getElementById('doneted-noakhali').innerText = totalDonation;
         document.getElementById('bank-money').innerText = remainingMoney;
         document.getElementById('input-noakhali').value = '';
+        showConfirmationModal();
     }
     else{
         alert('Invalid Donation Amount')
@@ -27,9 +31,12 @@ document.getElementById('btn-donate-feni').addEventListener('click', function(){
     if(!isNaN(donateForFeni) && donateForFeni > 0 && donateForFeni < bankMoney){
         const totalDonation = donetedFeni + donateForFeni;
         const remainingMoney = bankMoney - donateForFeni;
+
         document.getElementById('doneted-feni').innerText = totalDonation;
         document.getElementById('bank-money').innerText = remainingMoney;
         document.getElementById('input-feni').value = '';
+
+        showConfirmationModal();
     }
     else{
         alert('Invalid Donation Amount')
@@ -47,9 +54,12 @@ document.getElementById('btn-donate-injured').addEventListener('click', function
     if(!isNaN(donateForInjured) && donateForInjured > 0 && donateForInjured < bankMoney){
         const totalDonation = donetedInjured + donateForInjured;
         const remainingMoney = bankMoney - donateForInjured;
+
         document.getElementById('doneted-injured').innerText = totalDonation;
         document.getElementById('bank-money').innerText = remainingMoney;
         document.getElementById('input-injured').value = '';
+
+        showConfirmationModal();
     }
     else{
         alert('Invalid Donation Amount')
--- js/utilities.js
+++ js/utilities.js
@@ -1,15 +1,17 @@
+// Get input ID value
 function getInputValue(id){
     const inputDonation = document.getElementById(id).value;
     const numberOfDonationAmount = parseFloat(inputDonation)
     return numberOfDonationAmount;
 }
-
+// Get Text ID value
 function getTextValue(id){
     const doneteAmount = document.getElementById(id).innerText;
     const numberOfAmount = parseFloat(doneteAmount);
     return numberOfAmount
 }
 
+// Switching
 function showSection(id){
     document.getElementById('show-history').classList.add('hidden');
     document.getElementById('show-donation').classList.add('hidden');
@@ -17,10 +19,16 @@ function showSection(id){
 
     const footer = document.querySelector('footer')
     // Check which section is being shown
-    if(id=== 'show-history'){
+    if(id === 'show-history'){
         footer.classList.add('fixed', 'bottom-0');
     }
     else{
         footer.classList.remove('fixed', 'bottom-0');
     }
+}
+
+// Show Modal
+function showConfirmationModal(){
+    document.getElementById('my_modal_5').showModal();
+
 }
